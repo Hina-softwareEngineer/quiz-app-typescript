2dc7f439b4967a44c610c1f2612c598890b532ed
HINA KHADIM <hinakhadim2002@gmail.com>
2020-08-07 13:38:06 +0500

    adding shuffle function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { shuffleArray } from "./utils/shuffleArray";
+import "./App.css";
 
 function App() {
+  const [datafetch, setDatafetch] = useState([{ question: "question" }]);
+  const [shuffledata, setShuffleData] = useState({});
+
+  const fetchData = async () => {
+    console.log("fetchData");
+    const data = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=multiple"
+    );
+    let response = await data.json();
+    setDatafetch(response.results);
+    // console.log(response, response.results, " response is good");
+    let arrayShuffle: object[] = shuffleArray(response.results);
+    setShuffleData(arrayShuffle);
+  };
+
+  console.log("hello", datafetch, shuffledata);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz App</h1>
+
+      <button onClick={fetchData}>Start</button>
+
+      {datafetch || datafetch.map((question) => <div>{question.question}</div>)}
+
+      <h2>Shuffle</h2>
+      {/* {shuffledata && shuffledata.map((question) => <div>{question.question}</div>)} */}
     </div>
   );
 }
---
+++ src/utils/shuffleArray.tsx
@@ -0,0 +1,12 @@
+export const shuffleArray = (array: object[]) => {
+  let arrayLength = array.length;
+
+  for (var i = 0; i < arrayLength; i++) {
+    let randomIndex = Math.floor(Math.random() * 9 + 1);
+
+    let temp = array[i];
+    array[i] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+  return array;
+};
